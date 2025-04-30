754fd0e5f39b8f033495cb7072091393cdbac453
Abhirup Kumar Ghosh <143246270+Abhirup56@users.noreply.github.com>
2025-05-01 00:14:45 +0530

    Update server.js

--- backend2/SocketIO/server.js
+++ backend2/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:4001",
+    origin: "https://chatsphereapp-jp70.onrender.com",
     methods: ["GET", "POST"],
   },
 });
