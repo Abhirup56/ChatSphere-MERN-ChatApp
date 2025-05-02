e4ad215a0588f704ed976c143953ae4d9d273522
Abhirup Kumar Ghosh <143246270+Abhirup56@users.noreply.github.com>
2025-05-02 20:29:54 +0530

    Update user.model.js

--- backend2/models/user.model.js
+++ backend2/models/user.model.js
@@ -16,10 +16,6 @@ const userSchema = mongoose.Schema({
       type: String,
       required: true,
     },
-    confirmpassword: {
-      type: String,
-      required: true
-    },
     gender: {
       type: String,
       enum: ["Male", "Female", "Others"],
